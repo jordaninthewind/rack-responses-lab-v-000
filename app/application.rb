ebdef62801a6423c3995acac3e68c691268c5877
Jordan Kline <jordan.kline@gmail.com>
2018-02-04 22:59:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,11 +6,11 @@ class Application
     resp = Rack::Response.new
     # binding.pry
     current_hour = Time.new.hour
-    
-    if current_hour > 12
-      resp.write "Good Afternoon!"
-    elsif Time.new.
+
+    if current_hour < 12
       resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
     end
 
     resp.finish
