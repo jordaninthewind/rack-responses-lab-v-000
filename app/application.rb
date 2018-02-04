c7b888524c90e8ee3a733c85e3e1e109cdd43236
Jordan Kline <jordan.kline@gmail.com>
2018-02-04 23:02:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     # binding.pry
-    current_hour = Time.new.hour
+    current_hour = Time.new(time).hour
 
     if current_hour < 12
       resp.write "Good Morning"
